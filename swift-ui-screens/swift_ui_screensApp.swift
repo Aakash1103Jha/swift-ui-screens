42698157af9401f9f09905d0cff7445e6c5f33e1
Aakash Jha <aakash.jha1103@gmail.com>
2022-03-29 20:15:09 +0530

    Initial Commit

---
+++ swift-ui-screens/swift_ui_screensApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_ui_screensApp.swift
+//  swift-ui-screens
+//
+//  Created by Aakash Jha on 29/03/22.
+//
+
+import SwiftUI
+
+@main
+struct swift_ui_screensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
